7e7c4a3936b87513d241a8927ac6d3482fc82b4f
Nessiah919 <nesmith.charlesii@gmail.com>
2021-05-05 19:25:33 -0400

    created an update method in the views.py file that targets the id of a superhero and deletes from the database

--- Superhero_creator/superheroes/urls.py
+++ Superhero_creator/superheroes/urls.py
@@ -5,5 +5,7 @@ app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('display/<int:superhero_id>/', views.detail, name='detail'),
-    path('create/', views.create, name='create_superhero')
+    path('create/', views.create, name='create_superhero'),
+    path('update/<int:superhero_id>/', views.update, name='update_superhero'),
+    path('delete/<int:superhero_id>/', views.delete, name="delete")
 ]
--- Superhero_creator/superheroes/views.py
+++ Superhero_creator/superheroes/views.py
@@ -27,8 +27,34 @@ def create(request):
         primary_ability = request.POST.get('primary_ability')
         secondary_ability = request.POST.get('secondary_ability')
         catchphrase = request.POST.get('catchphrase')
-        new_superhero = Superhero(name=name, alter_ego_name=alter_ego, primary_ability=primary_ability, secondary_ability=secondary_ability, catchphrase=catchphrase)
+        new_superhero = Superhero(name=name, alter_ego_name=alter_ego, primary_ability=primary_ability,
+                                  secondary_ability=secondary_ability, catchphrase=catchphrase)
         new_superhero.save()
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request, 'superheroes/create.html')
+
+
+def update(request, superhero_id):
+    superhero = Superhero.objects.get(pk=superhero_id)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        catchphrase = request.POST.get('catchphrase')
+        superhero.name = name
+        superhero.alter_ego_name = alter_ego
+        superhero.primary_ability = primary_ability
+        superhero.secondary_ability = secondary_ability
+        superhero.category = catchphrase
+        superhero.save()
+        return HttpResponseRedirect(f'/superheroes/display/{superhero.id}')
+    else:
+        return render(request, 'superheroes/index.html')
+
+
+def delete(request, superhero_id):
+    superhero = Superhero.objects.get(pk=superhero_id)
+    superhero.delete()
+    return HttpResponseRedirect(reverse('superheroes:index'))
